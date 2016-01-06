1b0304c9cc5c9dbd17264c69a1bd345bb0d6e59e
JohnBobSmith <myrunescapeemail609@gmail.com>
2016-01-06 10:13:50 -0700

    CLI arg improvements, bug fixes.

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -14,11 +14,10 @@ int main(int argc, char **argv)
 	char piece = '-';	//current players player piece.
 	
 	if (argc < 2) {
-		std::cout << "Please enter how many players you would like. 1 or 2.\n";
-		std::cout << "Also enter which piece player one is. NO spaces\n";
+		std::cout << "Please enter a valid CLI argument, with NO hypens. \n";
 		std::cout << "Example: ./tictactoe 20 for two player game, random piece.\n";
 		std::cout << "Additional help will come when I'm closer to releasing game fully...\n";
-		return 0;
+		return -2; //Bad CLI arg or lack of one.
 	}
 	
 	//Welcome message/banner
@@ -34,26 +33,32 @@ int main(int argc, char **argv)
 			std::cout << "Player two, your piece is " << player2.playerPiece << "\n";
 			player.isTwoHumanPlayer = true;
 		} else {
-			std::cout << "Player one, your piece is O\n";
-			std::cout << "Player two, your piece is X\n";
 			player.playerPiece = 'O';
 			player2.playerPiece = 'X';
+			std::cout << "Player one, your piece is " << player.playerPiece << "\n";
+			std::cout << "Player two, your piece is " << player2.playerPiece << "\n";
 			player.isTwoHumanPlayer = true;
 		}
-	}
-	
-	if (strtol(argv[1], NULL, 10) == 21) {
-		std::cout << "Starting 2 player game, player on is X.\n";
+	} else if (strtol(argv[1], NULL, 10) == 21) {
 		player.playerPiece = 'X';
 		player2.playerPiece = 'O';
+		std::cout << "Starting 2 player game, fixed peices.\n";
+		std::cout << "Player one, your piece is " << player.playerPiece << "\n";
+		std::cout << "Player two, your piece is " << player2.playerPiece << "\n";
 		player.isTwoHumanPlayer = true;
-	}
-	
-	if (strtol(argv[1], NULL, 10) == 22) {
-		std::cout << "Starting 2 player game, player one is O.\n";
+	} else if (strtol(argv[1], NULL, 10) == 22) {
 		player.playerPiece = 'O';
 		player2.playerPiece = 'X';
+		std::cout << "Starting 2 player game, fixed peices.\n";
+		std::cout << "Player one, your piece is " << player.playerPiece << "\n";
+		std::cout << "Player two, your piece is " << player2.playerPiece << "\n";
 		player.isTwoHumanPlayer = true;
+	} else {
+		std::cout << "Error. Invalid CLI argument. Arguments are: 20 | 21 | 22\n";
+		std::cout << "Please use one of the above when running the program.\n";
+		std::cout << "For example, ./tictactoe 20\n";
+		std::cout << "Now exiting so you can retry.\n";
+		return -2; //bad CLI arg or lack of one.
 	}
 
 	while (game.isRunning) {
@@ -64,7 +69,11 @@ int main(int argc, char **argv)
 			if (player.isPlayerTurn) {
 				piece = player.playerPiece;
 				std::cout << "Player one, enter the number of the square you want.\n";
-				userInput = player.get_input();	
+				userInput = player.get_input();
+				if (userInput == -1) {
+					std::cout << "\nExit requested. Bye!\n";
+					return -1; //Error or user exit.
+				}
 				if  (game.make_move(userInput, player.playerPiece) == 1) {
 					std::cout << "Overlap detected... \n";
 					std::cout << "Player 1, try again.\n";
@@ -76,6 +85,10 @@ int main(int argc, char **argv)
 				piece = player2.playerPiece;
 				std::cout << "Player two, enter the number of the square you want.\n";
 				userInput = player.get_input();
+				if (userInput == -1) {
+					std::cout << "\nExit requested. Bye!\n";
+					return -1; //Error or user exit.
+				}
 				if  (game.make_move(userInput, player2.playerPiece) == 1) {
 					std::cout << "Overlap detected... \n";
 					std::cout << "Player 2, try again.\n";
@@ -90,7 +103,6 @@ int main(int argc, char **argv)
 		//Whoever wins first wins first, there is no other option.
 		//You cannot tie for win in tic tac toe, but you can draw
 		//and/or stalemate the game. <--- happens regularly... stalemates :Z
-		/*
 		if (game.is_victory(player.playerPiece)) {
 			std::cout << "\n\nPlayer one wins!\n\n";
 			std::cout << "Now exiting on grounds of happy victory ;)\n\n";
@@ -102,10 +114,11 @@ int main(int argc, char **argv)
 			std::cout << "Now exiting on grounds of happy victory ;)\n\n";
 			return 0;
 		}
-		*/
+		
 		if (game.isStalemate) {
 			std::cout << "\n\nNobody wins!\n\n";
 			std::cout << "Now exiting on grounds of an angry stalemate. ARgh!\n\n";
+			return 0;
 		}
 	}
 }
